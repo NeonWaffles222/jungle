f6125d3c64bb66527a4a0a06c10d07a887a67596
Noah McMillan <noahm27@gmail.com>
2023-09-23 01:58:32 -0700

    Admins can now add categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/new'
+    get 'categories/show' 
+    resources :categories
+  end
   get 'about/show'
   root to: 'products#index'
 
